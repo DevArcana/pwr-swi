d081894246b1bf4dd07817a41fc32e6ef5d69259
Dasarro <wicia0202@wp.pl>
2023-04-03 20:54:44 +0200

    Everything what I've done

--- redownload_datasets.sh
+++ redownload_datasets.sh
@@ -6,7 +6,7 @@ cd datasets || exit
 
 for i in $(seq -w 1 12); do
   curl https://database.lichess.org/standard/lichess_db_standard_rated_2013-"$i".pgn.zst -o lichess_db_standard_rated_2013-"$i".pgn.zst
-  pzstd -d lichess_db_standard_rated_2013-"$i".pgn.zst
+  zstd -d lichess_db_standard_rated_2013-"$i".pgn.zst
   rm lichess_db_standard_rated_2013-"$i".pgn.zst
 done
 
